7be9cafc0c296409f2300530b8913c24873c5c43
morampudisunil <morampudisunil492@gmail.com>
2024-05-05 21:07:33 +0530

    Initial commit

---
+++ sunil.sh
@@ -0,0 +1,20 @@
+
+
+# Navigate to your project directory
+
+
+# Initialize a Git repository
+git init
+cat ds.txt sd.txt df.txt
+# Add all files to the repository
+git add .
+
+# Commit the changes
+git commit -m "Initial commit"
+
+# Add the remote repository URL
+git remote add origin https://github.com/DevopsPracticeSunil/pushing.git
+
+# Push your code to GitHub
+git push -u origin master
+
